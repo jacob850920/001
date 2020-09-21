7b3a1b4e2d07075795fa052c0fc2b3299adc0165
ggcgm <70868223+ggcgm@users.noreply.github.com>
2020-09-21 08:18:49 +0800

    Add files via upload

---
+++ STRING.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 07:54:19 2020
+
+@author: cis-user
+"""
+# --------第一題-----------------------------
+# string='I love you'
+
+# s1=string[:1]
+# s2=string[2:6]
+# s3=string[7:]
+
+# print(s1)
+# print(s2)
+# print(s3)
+#----------第二題----------------------------------------------
+# string='I love you'
+# s1=string[::-1]
+
+# print(s1)
+#--------第三題------------------------------------------
+
+# string='I love you'
+# s='so'
+# s1=s*100
+# m='much'
+# print(string,s1,m)
+#----------第四題-------------------------------------------
+
+# string='I love you'
+# import random as r
+# r=r.randint(1,100)
+# s='so'
+# x=s*r
+# m='much'
+# print(string,x,m)
