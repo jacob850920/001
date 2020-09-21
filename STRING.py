3d107f9ff44fe1f5169b5d60242691288da03ab2
ggcgm <70868223+ggcgm@users.noreply.github.com>
2020-09-21 09:22:20 +0800

    Add files via upload

--- STRING.py
+++ STRING.py
@@ -10,10 +10,10 @@ Created on Mon Sep 21 07:54:19 2020
 # s1=string[:1]
 # s2=string[2:6]
 # s3=string[7:]
+# string=s1+" "+s2+" "+s3
 
-# print(s1)
-# print(s2)
-# print(s3)
+# print(string)
+
 #----------第二題----------------------------------------------
 # string='I love you'
 # s1=string[::-1]
@@ -22,10 +22,11 @@ Created on Mon Sep 21 07:54:19 2020
 #--------第三題------------------------------------------
 
 # string='I love you'
-# s='so'
+# s=' so'
 # s1=s*100
 # m='much'
-# print(string,s1,m)
+# string=string+s+s1+m
+# print(string)
 #----------第四題-------------------------------------------
 
 # string='I love you'
