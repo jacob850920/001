9609501906e7389a9b7fff39801a624f1086e351
ggcgm <70868223+ggcgm@users.noreply.github.com>
2020-09-28 08:31:12 +0800

    Add files via upload

---
+++ 9X9.py
@@ -0,0 +1,56 @@
+# # -*- coding: utf-8 -*-
+# """
+# Created on Mon Sep 28 07:51:03 2020
+
+# @author: cis-user
+# """
+# #第一題=================
+# #1-1
+# score=['小徐',5,9,6,8,7,10,6]
+# s1=score[1:]
+# a=max(s1)
+# print(a)
+
+# #1-2
+# score=['小徐',5,9,6,8,7,10,6]
+# s1=score[1:]
+# b=min(s1)
+# print(b)
+
+# #1-3
+# score=['小徐',5,9,6,8,7,10,6]
+# s1=score[1:]
+# s1.sort(reverse=True)
+# sorted(s1)
+# x,y,z,q,w,e,r=s1
+# print(x,y,z)
+
+# #1-4
+# score=['小徐',5,9,6,8,7,10,6]
+# s1=score[1:]
+# s1.sort(reverse=True)
+# sorted(s1)
+# x,y,z,q,w,e,r=s1
+# print(w,e,r)
+# #1-5
+# score=['小徐',5,9,6,8,7,10,6]
+# s1=score[1:]
+# s2=sum(s1)/len(s1)
+# print(s2)
+
+# #第二題====================================
+# for i in range(1,10):
+#       for j in range(1,10):
+#         print("%d*%d=%2d" % (i,j,i*j),end=" ")
+#       print("")
+
+      
+# #第三題===================================
+
+# import random as r
+# r= r.randint(1,100)
+# a='I love you'
+# print(a,end=' ')
+# for  b  in  range(r):
+#         s='so'
+#         print(s,end=' ')
