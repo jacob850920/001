b28d4285d0542d4e9eb804222bd31f1ea3a37103
ggcgm <70868223+ggcgm@users.noreply.github.com>
2020-10-26 11:50:09 +0800

    Add files via upload

--- 找質數.py
+++ 找質數.py
@@ -4,13 +4,12 @@ Created on Mon Oct 26 09:30:40 2020
 
 @author: cis-user
 """
-print("↓請輸入n，找出2~n2的質數↓",end='')
+print("↓請輸入n，找出2~n的質數↓",end='')
 n=eval(input())
 for i in range(2,n):
     for j in range(2,i):
      
         if i%j == 0:
-            #print(n, 'is not prime')
             break
     else:
         print(i, '是質數')
