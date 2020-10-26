41777e7702e0ebe6547f081281caf6d10a08313f
ggcgm <70868223+ggcgm@users.noreply.github.com>
2020-10-26 10:55:28 +0800

    Add files via upload

--- 找質數.py
+++ 找質數.py
@@ -4,31 +4,13 @@ Created on Mon Oct 26 09:30:40 2020
 
 @author: cis-user
 """
-import random
-ansMax=100
-ansMin=0
-guessCount=0
-guessAnswer=random.randint(1,99)
-guessNum=0
-while guessNum != guessAnswer:
-    #print('Guess Count=',guessCount)
-    print(ansMin, '< ? <',ansMax)
-    guessNum=eval(input())
-    guessCount +=1
-    if guessNum > ansMin and guessNum < ansMax:
-        #print('guess count =',guessCount)
-        #if guessNum == guessAnswer:
-        #    print('bingo answer is',guessAnswer)
-        #    break
-        if guessNum > guessAnswer:
-            print('再小一點')
-            print('已猜次數',guessCount)
-            ansMax = guessNum
-        elif guessNum < guessAnswer:
-            print('再大一點')
-            print('已猜次數',guessCount)
-            ansMin = guessNum
-        else:
-            print('賓果猜對了','總猜次數',guessCount)
+print("↓請輸入n，找出2~n2的質數↓",end='')
+n=eval(input())
+for i in range(2,n):
+    for j in range(2,i):
+     
+        if i%j == 0:
+            #print(n, 'is not prime')
+            break
     else:
-        print('超出範圍')
+        print(i, '是質數')
