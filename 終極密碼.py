ff43afd384daf626e8c9abc8ac39534c815a867e
ggcgm <70868223+ggcgm@users.noreply.github.com>
2020-10-26 11:06:08 +0800

    Add files via upload

--- 終極密碼.py
+++ 終極密碼.py
@@ -8,25 +8,25 @@ Created on Mon Oct 26 09:45:28 2020
 import random
 ansMax=100
 ansMin=0
-
+guessCount=0
 guessAnswer=random.randint(1,99)
 guessNum=0
 while guessNum != guessAnswer:
     
-    print(ansMin, '< ? <',ansMax)
+    print(ansMin, '< ? <',ansMax,end='')
     guessNum=eval(input())
-    
+    guessCount +=1
     if guessNum > ansMin and guessNum < ansMax:
-        
+       
         if guessNum > guessAnswer:
             print('再小一點')
-            
+            print('已猜次數',guessCount)
             ansMax = guessNum
         elif guessNum < guessAnswer:
             print('再大一點')
-           
+            print('已猜次數',guessCount)
             ansMin = guessNum
         else:
-            print('賓果猜對了')
+            print('賓果猜對了',guessCount)
     else:
-        print("超出範圍")
+        print('超出範圍')
