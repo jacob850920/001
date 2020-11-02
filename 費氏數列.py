34336ce7b420c80af80e9980be3f8835254f9c1e
ggcgm <70868223+ggcgm@users.noreply.github.com>
2020-11-02 17:00:35 +0800

    Add files via upload

--- 費氏數列.py
+++ 費氏數列.py
@@ -10,12 +10,13 @@ Created on Mon Nov  2 14:25:30 2020
 from decimal import *
 getcontext().prec = 6000
 print("↓f(n),請輸入n=?↓",end='')
-n = int(input())
+n = int(input())-1
 
 sqrt_5 = Decimal(5).sqrt()
 x=sqrt_5/5 * (((1+sqrt_5)/2)**n - ((1-sqrt_5)/2)**n)
-   
+print("公式解的答案為：")   
 print(f"{x:.0f}")
+
 
 # 驗證用，非遞迴解法
 def f(n):
