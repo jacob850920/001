47f751795fb89f188d5b69593ef7262f83eee214
ggcgm <70868223+ggcgm@users.noreply.github.com>
2020-12-21 10:12:14 +0800

    Add files via upload

--- MOMO.py
+++ MOMO.py
@@ -15,7 +15,7 @@ from selenium.webdriver.support.ui import Select
 
 
 
-key="iphone12"
+key="switch"
 url="https://www.momoshop.com.tw/"
 options=webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
@@ -56,7 +56,7 @@ with open('MOMO爬蟲.csv','w+',newline='', encoding="utf-8-sig") as csvfile:   
             
                 writer.writerow([search_name[i].text,"MOMO購物網",search_price[i].text,"https://www.momoshop.com.tw"+search_url[i].get('href')])
                 
-        driver.get("https://www.momoshop.com.tw/search/searchShop.jsp?keyword=iphone12&searchType=1&curPage=1&_isFuzzy=0&showType=chessboardType")
+        # driver.get("https://www.momoshop.com.tw/search/searchShop.jsp?keyword=iphone12&searchType=1&curPage=1&_isFuzzy=0&showType=chessboardType")
         driver.find_element_by_xpath(u"(//a[contains(text(),'下一頁')])[2]").click()
         # time.sleep(2)
         
