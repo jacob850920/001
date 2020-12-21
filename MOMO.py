809c864358c0d3c7e47c157df1e9a8f7956a5573
ggcgm <70868223+ggcgm@users.noreply.github.com>
2020-12-21 10:13:25 +0800

    Add files via upload

--- MOMO.py
+++ MOMO.py
@@ -56,7 +56,7 @@ with open('MOMO爬蟲.csv','w+',newline='', encoding="utf-8-sig") as csvfile:   
             
                 writer.writerow([search_name[i].text,"MOMO購物網",search_price[i].text,"https://www.momoshop.com.tw"+search_url[i].get('href')])
                 
-        # driver.get("https://www.momoshop.com.tw/search/searchShop.jsp?keyword=iphone12&searchType=1&curPage=1&_isFuzzy=0&showType=chessboardType")
+       
         driver.find_element_by_xpath(u"(//a[contains(text(),'下一頁')])[2]").click()
         # time.sleep(2)
         
