7f26a2e6288550d2578d7deb79a73df42bc9e45e
ggcgm <70868223+ggcgm@users.noreply.github.com>
2020-12-27 08:24:32 +0800

    Add files via upload

--- 蝦皮爬蟲.py
+++ 蝦皮爬蟲.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec 22 10:15:48 2020
+Created on Sat Dec 26 19:44:12 2020
 
-@author: 90053
+@author: USER
 """
+
 from selenium import webdriver
 import csv
 from bs4 import BeautifulSoup
@@ -13,8 +14,8 @@ from selenium.webdriver.support.ui import Select
 import requests
 
 
-key="switch主機"
-url="https://shopee.tw/search?keyword="+key
+key="iphone 12 mini 64g"
+url="https://shopee.tw/mall/search?keyword="+key
 options=webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 driver=webdriver.Chrome(chrome_options=options)
@@ -22,31 +23,32 @@ driver.get(url)
 
 time.sleep(3)
 
-page=0
+
 with open('蝦皮爬蟲.csv','w+',newline='', encoding="utf-8-sig") as csvfile:   #解決多一空行 newline=''
     writer = csv.writer(csvfile)
     writer.writerow(('商品名','網站','價格','連結'))
     
     
 
-    for i in range(1):   
-        page+=1
-        html = driver.page_source
-        sp=BeautifulSoup(html,"html.parser")
-        search_name=sp.select("div._1NoI8_")#商品名
-        search_price=sp.select("div._1w9jLI > span._341bF0")#價格
-        search_url=sp.select("div.col-xs-2-4 > div > a")#網址
+      
         
-        for i in range(15):  
-                print(page)
-                print(search_name[i].text,end=' ')
-                print(search_price[i].text)
-                print("https://shopee.tw" + search_url[i].get('href'))
+    html = driver.page_source
+    sp=BeautifulSoup(html,"html.parser")
+    search_name=sp.select("div._1NoI8_")#商品名
+    search_price=sp.find_all("div", class_="_1w9jLI _37ge-4 _2ZYSiu")#價格
+    search_url=sp.select("div.col-xs-2-4 > div > a")#網址
+        
+    for i in range(len(search_name)):  
+        print(i+1)
+        print(search_name[i].text,end=' ')
+        print("[蝦皮商城]",end=' ')
+        print(search_price[i].text,end=' ')
+        print("https://shopee.tw" + search_url[i].get('href'))
                 
             
-                writer.writerow([search_name[i].text,"蝦皮購物",search_price[i].text,"https://shopee.tw"+search_url[i].get('href')])
+        writer.writerow([search_name[i].text,"蝦皮商城",search_price[i].text,"https://shopee.tw"+search_url[i].get('href')])
                 
 driver.close()               #關閉瀏覽器
 
 
-sys.exit                              
+sys.exit 
